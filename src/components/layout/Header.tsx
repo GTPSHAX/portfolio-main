e006addd632082e90a9ca558b14e432243529e04
GTPSHAX <rafiehasannudin@gmail.com>
2026-03-16 01:43:45 +0700

    feat: add Header component with Navbar integration

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/layout/Navbar";
+
+interface HeaderProps {
+  className?: string;
+}
+
+export default function Header({ className }: HeaderProps) {
+  return (
+    <header className={cn("sticky top-0 z-50 border-white", className)}>
+      <Navbar />
+    </header>
+  )
+}
