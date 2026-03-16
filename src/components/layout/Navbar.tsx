8caf9299aaa8f796150bf5a4c61f266ed9969376
GTPSHAX <rafiehasannudin@gmail.com>
2026-03-16 14:11:36 +0700

    feat: remove hardcoded items from Navbar component

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -14,37 +14,6 @@ interface NavbarProps {
 }
 
 const Navbar = async () => {
-  const items = [
-    {
-      label: "About",
-      bgColor: "#0D0716",
-      textColor: "#fff",
-      links: [
-        { label: "Company", ariaLabel: "About Company", href: "#" },
-        { label: "Careers", ariaLabel: "About Careers", href: "#" },
-      ],
-    },
-    {
-      label: "Projects",
-      bgColor: "#170D27",
-      textColor: "#fff",
-      links: [
-        { label: "Featured", ariaLabel: "Featured Projects", href: "#" },
-        { label: "Case Studies", ariaLabel: "Project Case Studies", href: "#" },
-      ],
-    },
-    {
-      label: "Contact",
-      bgColor: "#271E37",
-      textColor: "#fff",
-      links: [
-        { label: "Email", ariaLabel: "Email us", href: "#" },
-        { label: "Twitter", ariaLabel: "Twitter", href: "#" },
-        { label: "LinkedIn", ariaLabel: "LinkedIn", href: "#" },
-      ],
-    },
-  ];
-
   const navbarFrontmatter = (navbarContent as { frontmatter?: NavbarProps }).frontmatter;
 
   return (
