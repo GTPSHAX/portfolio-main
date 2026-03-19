2c996c9da7e69e666164fc13df955d0ee04f1c87
GTPSHAX <rafiehasannudin@gmail.com>
2026-03-19 09:16:57 +0700

    feat: replace GlassNav with MonospaceNav component for improved navigation

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,8 @@
 "use server";
 
-import { GlassNav, GlassNavItem } from "@/components/GlassNav";
 import { frontmatter } from "#/content/navbar.mdx";
 import Link from "next/link";
+import { MonospaceNav, MonospaceNavBrand, MonospaceNavItem, MonospaceNavItemList } from "../MonospaceNav";
 
 interface NavbarProps {
   label: string;
@@ -17,19 +17,28 @@ const Navbar = async () => {
   const navbarFrontmatter = frontmatter as NavbarProps;
 
   return (
-    <GlassNav label={navbarFrontmatter?.label ?? "Default Label"} ariaLabel="Main navigation">
-      {navbarFrontmatter?.items.map((item) => (
-        <GlassNavItem key={item.label} ariaLabel={item.ariaLabel}>
-          <Link
-            href={item.href}
-            className="flex h-full w-full items-center justify-center px-4 py-2"
-            aria-label={item.ariaLabel}
-          >
-            {item.label}
-          </Link>
-        </GlassNavItem>
-      ))}
-    </GlassNav>
+    <MonospaceNav
+      ariaLabel={navbarFrontmatter.label}
+      className=""
+    >
+      <MonospaceNavBrand href="/" ariaLabel="Brand Logo">
+        {navbarFrontmatter.label}
+      </MonospaceNavBrand>
+
+      <MonospaceNavItemList ariaLabel="Navigation Items">
+        {navbarFrontmatter.items.map((item) => (
+          <MonospaceNavItem key={item.label} ariaLabel={item.ariaLabel}>
+            <Link
+              href={item.href}
+              className="flex-1 text-center"
+              aria-label={item.ariaLabel}
+            >
+              {item.label}
+            </Link>
+          </MonospaceNavItem>
+        ))}
+      </MonospaceNavItemList>
+    </MonospaceNav>
   );
 };
 
