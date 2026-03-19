00b4101d3d4ceb796637cddb0a000d8ac37468b5
GTPSHAX <rafiehasannudin@gmail.com>
2026-03-20 00:08:08 +0700

    refactor: restructure greetings and social links layout for improved responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import { outfit, robotoMono, rubikGlitch, rubikMonoOne } from "@/fonts/google";
 import Opening from "@/components/Opening";
 import MainLayout from "@/components/layout/MainLayout";
 import Image from "next/image";
-import { bitsumis, blastimoSans, conflict3040, crackedCode, drunkText } from "@/fonts/local";
+import {
+  bitsumis,
+  blastimoSans,
+  conflict3040,
+  crackedCode,
+  drunkText,
+} from "@/fonts/local";
 import { frontmatter } from "#/content/index.mdx";
 import { getSocialIcon, SocialIconType } from "@/lib/server/utils";
 import Link from "next/link";
@@ -51,52 +57,83 @@ export default async function Home() {
 
         <div className="container mx-auto px-6 pt-10 h-full">
           {/* Hero Content */}
-          <div className="relative w-full h-full">
-            {/* Greetings Text Container */}
-            <div className="z-2 relative flex flex-col items-start gap-3 mt-10 md:mt-20">
-              {/* Badge */}
-              <div className={`flex items-center gap-3 px-3 py-1.5 border border-accent backdrop-blur-md ${indexFrontmatter.openToWork ? 'bg-green-500/50' : 'bg-red-500/50'}`}>
-                <div aria-hidden="true" className={`w-2 h-2 animate-pulse ${indexFrontmatter.openToWork ? 'bg-green-700' : 'bg-red-700'}`} />
-                <span className={`${robotoMono.className} text-xs md:text-sm font-semibold uppercase tracking-widest`}>
-                  {indexFrontmatter.openToWork ? 'Open to Work' : 'Not Open to Work'}
-                </span>
-              </div>
-              
-              {/* Main Headline */}
-              <div className="flex flex-col mt-3">
-                <p className={`${outfit.className} text-2xl md:text-3xl font-light`}>
-                  Hello, I'm
-                </p>
-                <h1 className={`${bitsumis.className} font-semibold text-[3.4rem] md:text-6xl lg:text-[5rem] leading-[0.7]`} dangerouslySetInnerHTML={{ __html: indexFrontmatter.name }} />
+          <div className="relative w-full h-full flex items-center">
+            <div className="relative z-2 pt-10 w-1/2 h-full">
+              {/* Greetings Text Container */}
+              <div className="flex flex-col items-start gap-3">
+                {/* Badge */}
+                <div
+                  className={`flex items-center gap-3 px-3 py-1.5 border border-accent backdrop-blur-md ${indexFrontmatter.openToWork ? "bg-green-500/50" : "bg-red-500/50"}`}
+                >
+                  <div
+                    aria-hidden="true"
+                    className={`w-2 h-2 animate-pulse ${indexFrontmatter.openToWork ? "bg-green-700" : "bg-red-700"}`}
+                  />
+                  <span
+                    className={`${robotoMono.className} text-xs md:text-sm font-semibold uppercase tracking-widest`}
+                  >
+                    {indexFrontmatter.openToWork
+                      ? "Open to Work"
+                      : "Not Open to Work"}
+                  </span>
+                </div>
 
-                {/* sub-text - roles */}
-                <ul aria-label="My Roles" className="flex flex-wrap gap-3 mt-2 divide-accent divide-x-2 md:max-w-[40vw]">
-                  {indexFrontmatter.roles && indexFrontmatter.roles.map((role, index) => (
-                    <li key={index} aria-label={role} className={`${outfit.className} text-md md:text-lg lg:text-[1.5rem] mt-2 opacity-80 tracking-wide pr-3`}>
-                      {role}
-                    </li>
-                  ))}
-                </ul>
+                {/* Main Headline */}
+                <div className="flex flex-col mt-3">
+                  <p
+                    className={`${outfit.className} text-2xl md:text-3xl font-light`}
+                  >
+                    Hello, I'm
+                  </p>
+                  <h1
+                    className={`${bitsumis.className} font-semibold text-[3.4rem] md:text-6xl lg:text-[5rem] leading-[0.7]`}
+                    dangerouslySetInnerHTML={{ __html: indexFrontmatter.name }}
+                  />
+
+                  {/* sub-text - roles */}
+                  <ul
+                    aria-label="My Roles"
+                    className="flex flex-wrap gap-3 mt-2 divide-accent divide-x-2 md:max-w-[40vw]"
+                  >
+                    {indexFrontmatter.roles &&
+                      indexFrontmatter.roles.map((role, index) => (
+                        <li
+                          key={index}
+                          aria-label={role}
+                          className={`${outfit.className} text-md md:text-lg lg:text-[1.5rem] mt-2 opacity-80 tracking-wide pr-3`}
+                        >
+                          {role}
+                        </li>
+                      ))}
+                  </ul>
+                </div>
               </div>
             </div>
-
-            {/* Social Links */}
-            <nav className="absolute top-1/3 z-3 md:z-auto md:top-0 right-0 p-6 flex flex-col items-center gap-4 pointer-events-auto group transition-opacity duration-300 border border-accent bg-foreground md:border-none md:bg-transparent">
-              <div aria-hidden="true" className="h-5 w-1 bg-background group-hover:opacity-70" />
-              {indexFrontmatter.socialLinks.map((link, index) => (
-                <Link
-                  key={index}
-                  href={link.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label={link.ariaLabel}
-                  className="group-hover:not-hover:opacity-70"
-                >
-                  {getSocialIcon(link.type, 30)}
-                </Link>
-              ))}
-              <div aria-hidden="true" className="h-15 w-1 bg-background group-hover:opacity-70" />
-            </nav>
+            <div className="relative z-2 pt-10 w-1/2 h-full">
+              {/* Social Links */}
+              <nav className="absolute bottom-20 z-3 md:z-auto md:top-0 right-0 p-6 flex flex-col items-center gap-4 pointer-events-auto group transition-opacity duration-300 border border-accent bg-foreground md:border-none md:bg-transparent">
+                <div
+                  aria-hidden="true"
+                  className="h-5 w-1 bg-background group-hover:opacity-70"
+                />
+                {indexFrontmatter.socialLinks.map((link, index) => (
+                  <Link
+                    key={index}
+                    href={link.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label={link.ariaLabel}
+                    className="group-hover:not-hover:opacity-70"
+                  >
+                    {getSocialIcon(link.type, 30)}
+                  </Link>
+                ))}
+                <div
+                  aria-hidden="true"
+                  className="h-15 w-1 bg-background group-hover:opacity-70"
+                />
+              </nav>
+            </div>
 
             {/* Profile Image */}
             <Image
