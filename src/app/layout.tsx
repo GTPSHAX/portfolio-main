2ca5f75e2aac81e365925b7c840f3f2df52d2a3f
GTPSHAX <rafiehasannudin@gmail.com>
2026-03-22 22:22:07 +0700

    feat: integrate Terminal F4 font into layout for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/theme-provider";
+import { terminalF4 } from "@/fonts/local";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased`}>
+      <body className={`antialiased ${terminalF4.variable}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
