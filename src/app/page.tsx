fb2087e259dd62dea6673fceeb9c212cb3afceda
GTPSHAX <rafiehasannudin@gmail.com>
2026-03-24 21:25:45 +0700

    feat: enhance Home component layout with Noise effect and structured sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,34 @@
 "use server";
 
-import { outfit, robotoMono } from "@/fonts/google";
 import Opening from "@/components/Opening";
 import MainLayout from "@/components/layout/MainLayout";
 import { terminalF4 } from "@/fonts/local";
+import Noise from "@/components/Noise";
+
+// Sections
 import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
+import ProjectSection from "@/components/sections/ProjectSection";
 
 export default async function Home() {
-
   return (
-    <MainLayout className={`relative ${terminalF4.className}`}>
+    <MainLayout
+      opening={<Opening />}
+      className={`relative ${terminalF4.className}`}
+    >
+      <div className="fixed top-0 left-0 w-full h-full z-100 pointer-events-none">
+        <Noise
+          patternSize={250}
+          patternScaleX={2}
+          patternScaleY={2}
+          patternRefreshInterval={2}
+          patternAlpha={30}
+        />
+      </div>
+
       <HeroSection />
       <AboutSection />
-      
-      <div className="">
-        <div className="h-screen"></div>
-        <div className="h-screen"></div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptas
-        dignissimos earum eum vero, aliquid error temporibus voluptatem eligendi
-        quo placeat quos similique nemo amet inventore nobis ipsa sunt harum.
-      </div>
+      <ProjectSection />
     </MainLayout>
   );
 }
