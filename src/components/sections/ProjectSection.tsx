93e639577c9e951ce66f0821199a89390485434a
GTPSHAX <rafiehasannudin@gmail.com>
2026-03-24 23:11:44 +0700

    feat: replace static project header with StaggerChildren and StaggerItem components for enhanced animations

--- src/components/sections/ProjectSection.tsx
+++ src/components/sections/ProjectSection.tsx
@@ -3,6 +3,7 @@
 import { bitsumis } from "@/fonts/local";
 import { getAllProjects } from "@/lib/server/utils";
 import ProjectList from "./ProjectList";
+import StaggerChildren, { StaggerItem } from "@/components/StaggerChildren";
 
 export default async function ProjectSection() {
   const projects = await getAllProjects();
@@ -14,24 +15,34 @@ export default async function ProjectSection() {
     >
       <div className="container z-1 w-full h-full py-10 divide-accent/80">
         {/* Projects Header */}
-        <div className="flex flex-col gap-5 p-6">
-          <div className="flex items-center justify-center gap-2">
-            <span className="uppercase tracking-widest text-nowrap text-xs md:text-sm text-accent/70 hover:underline hover:decoration-accent/50 cursor-default w-fit">
-              # [03] / Projects
-            </span>
-            <div className="w-full border border-accent/70 border-dashed" />
-          </div>
-          <h2
-            className={`${bitsumis.className} text-4xl md:text-5xl leading-[0.9]`}
-            dangerouslySetInnerHTML={{ __html: "My Projects" }}
-          />
-          <p
-            className="text-lg md:text-xl text-background/60 leading-6"
-            dangerouslySetInnerHTML={{
-              __html: "Here are some of the projects I've worked on recently.",
-            }}
-          />
-        </div>
+        <StaggerChildren
+          className="flex flex-col gap-5 p-6"
+          staggerDelay={0.15}
+        >
+          <StaggerItem direction="down">
+            <div className="flex items-center justify-center gap-2">
+              <span className="uppercase tracking-widest text-nowrap text-xs md:text-sm text-accent/70 hover:underline hover:decoration-accent/50 cursor-default w-fit">
+                # [03] / Projects
+              </span>
+              <div className="w-full border border-accent/70 border-dashed" />
+            </div>
+          </StaggerItem>
+          <StaggerItem>
+            <h2
+              className={`${bitsumis.className} text-4xl md:text-5xl leading-[0.9]`}
+              dangerouslySetInnerHTML={{ __html: "My Projects" }}
+            />
+          </StaggerItem>
+          <StaggerItem>
+            <p
+              className="text-lg md:text-xl text-background/60 leading-6"
+              dangerouslySetInnerHTML={{
+                __html:
+                  "Here are some of the projects I've worked on recently.",
+              }}
+            />
+          </StaggerItem>
+        </StaggerChildren>
 
         {/* Project List */}
         <ProjectList projects={projects} />
